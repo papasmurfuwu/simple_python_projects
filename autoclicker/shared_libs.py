5eb2485224ae7f3b273a07a8fd861aa7f344a963
Wong Quentan <quentanwong@gmail.com>
2024-09-04 22:13:55 +0800

    Added tkinter app class, start and stop buttons

--- autoclicker/shared_libs.py
+++ autoclicker/shared_libs.py
@@ -1,6 +1,5 @@
 # Contains imported libraries and functions
-
-import pyautogui, threading, time, tkinter
+import pyautogui, threading, time, customtkinter
 from tkinter import * 
 from pynput import mouse
 
@@ -31,3 +30,58 @@ def autospace():
             
     listener.stop()
     print("Script terminated.")
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Auto Clicker 1.0')
+        self.minsize(400,200)
+
+        # Configure grid columns to have equal weight
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        
+        # Label "Click Interval"
+
+        self.start_button = customtkinter.CTkButton(self, text='Start', command=self.autoclick_callback)
+        self.start_button.grid(row=0, column=0, padx=20, pady=20, sticky='ew')
+
+        self.stop_button = customtkinter.CTkButton(self, text='Stop', command=self.stop_callback)
+        self.stop_button.grid(row=0, column=1, padx=20, pady=20, sticky='ew')
+        
+        self.bind('<C>', self.shift_c_pressed)
+        self.start = False
+
+    def autoclick_callback(self):
+        print(f'Autoclicker activated!')
+        self.start_button.configure(state=DISABLED)
+        self.stop_button.configure(state=NORMAL)
+        self.start = True
+
+    def stop_callback(self):
+        print('Autoclicker turned off...')
+        self.start_button.configure(state=NORMAL)
+        self.stop_button.configure(state=DISABLED)
+        self.start = False
+
+    def shift_c_pressed(self, event):
+        if not self.start:
+            self.autoclick_callback()
+            
+        else:
+            self.stop_callback()
+            
+
+
+
+    
+    
+   
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
+
