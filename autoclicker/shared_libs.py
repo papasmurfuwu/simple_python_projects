6bdc3ff54815f7b5e6b1cac107e3a83a11681285
Wong Quentan <quentanwong@gmail.com>
2024-09-04 23:20:23 +0800

    Added row for click interval input

--- autoclicker/shared_libs.py
+++ autoclicker/shared_libs.py
@@ -3,6 +3,8 @@ import pyautogui, threading, time, customtkinter, keyboard
 from tkinter import * 
 from pynput import mouse
 
+# Add 
+pyautogui.PAUSE = 10
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -14,14 +16,43 @@ class App(customtkinter.CTk):
         # Configure grid columns to have equal weight
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+        self.grid_columnconfigure(4, weight=1)
+        self.grid_columnconfigure(5, weight=1)
+        self.grid_columnconfigure(6, weight=1)
+        self.grid_columnconfigure(7, weight=1)
+
+        label_font = ('Calibri', 12)
+
+        # Create labels and entries for time input with specified font
+        self.input1 = customtkinter.CTkEntry(self, placeholder_text="0", justify="right", width=50, corner_radius=0, font=label_font)
+        self.input1.grid(row=0, column=0, padx=(5, 0), pady=5)
+        self.label1 = customtkinter.CTkLabel(self, text="Hours", font=label_font)
+        self.label1.grid(row=0, column=1, padx=(5, 5), pady=5)
         
-        # Label "Click Interval"
+
+        self.input2 = customtkinter.CTkEntry(self, placeholder_text="0", justify="right", width=50, corner_radius=0, font=label_font)
+        self.input2.grid(row=0, column=2, padx=(10, 0), pady=5)
+        self.label2 = customtkinter.CTkLabel(self, text="Minutes", font=label_font)
+        self.label2.grid(row=0, column=3, padx=(5, 5), pady=5)
+        
+        self.input3 = customtkinter.CTkEntry(self, placeholder_text="0", justify="right", width=50, corner_radius=0, font=label_font)
+        self.input3.grid(row=0, column=4, padx=(10, 0), pady=5)
+        self.label3 = customtkinter.CTkLabel(self, text="Seconds", font=label_font)
+        self.label3.grid(row=0, column=5, padx=(5, 5), pady=5)
+        
+        self.input4 = customtkinter.CTkEntry(self, placeholder_text="0", justify="right", width=50, corner_radius=0, font=label_font)
+        self.input4.grid(row=0, column=6, padx=(10, 0), pady=5)
+        self.label4 = customtkinter.CTkLabel(self, text="Milliseconds", font=label_font)
+        self.label4.grid(row=0, column=7, padx=(5, 5), pady=5)
+        
 
         self.start_button = customtkinter.CTkButton(self, text='Start (Shift+C)', command=self.start_callback)
-        self.start_button.grid(row=0, column=0, padx=20, pady=20, sticky='ew')
+        self.start_button.grid(row=2, column=0, columnspan=4, padx=20, pady=20, sticky='ew')
 
         self.stop_button = customtkinter.CTkButton(self, text='Stop (Shift+C)', command=self.stop_callback)
-        self.stop_button.grid(row=0, column=1, padx=20, pady=20, sticky='ew')
+        self.stop_button.grid(row=2, column=4, columnspan=4, padx=20, pady=20, sticky='ew')
         self.stop_button['state'] = 'disabled'
 
         keyboard.add_hotkey('shift+c', self.toggle_autoclicker)
@@ -30,7 +61,7 @@ class App(customtkinter.CTk):
 
     def autoclick(self):
         while self.start:
-            pyautogui.click(interval=0.00001)
+            pyautogui.click()
             
 
     def start_callback(self):
